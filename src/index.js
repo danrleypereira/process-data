d0fb45fce45d55461185fe1753a3b15fbe884743
Danrley Pereira <danrleywillian@gmail.com>
2024-09-14 04:00:11 -0300

    feat: verification digits functions

--- src/index.js
+++ src/index.js
@@ -3,7 +3,47 @@ const isCpfValid = (cpf) => {
   if (cleanedCpf.length !== 11) {
     return false;
   }
-  return true;
+  if (
+    isVerificationDigitsValid(cleanedCpf.slice(0, 9), cleanedCpf.slice(9, 11))
+  ) {
+    return true;
+  }
+  return false;
+};
+
+const isVerificationDigitsValid = (cpf, verificationDigits) => {
+  const firstDigit = calculateFirstDigit(cpf);
+  const secondDigit = calculateSecondDigit(cpf);
+
+  return `${firstDigit}${secondDigit}` === verificationDigits;
+};
+
+const calculateFirstDigit = (cpf) => {
+  const cpfArray = cpf.slice(0, 9).split("").map(Number);
+  const multiplicadores = [10, 9, 8, 7, 6, 5, 4, 3, 2];
+
+  const soma = cpfArray.reduce(
+    (acc, num, index) => acc + num * multiplicadores[index],
+    0
+  );
+
+  const resto = soma % 11;
+  return resto < 2 ? 0 : 11 - resto;
+};
+
+const calculateSecondDigit = (cpf) => {
+  const cpfArray = cpf.slice(0, 9).split("").map(Number);
+  cpfArray.push(calculateFirstDigit(cpf));
+
+  const multiplicadores = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+
+  const soma = cpfArray.reduce(
+    (acc, num, index) => acc + num * multiplicadores[index],
+    0
+  );
+
+  const resto = soma % 11;
+  return resto < 2 ? 0 : 11 - resto;
 };
 
 const isCnpjValid = (cnpj) => {
@@ -17,4 +57,6 @@ const isCnpjValid = (cnpj) => {
 module.exports = {
   isCpfValid,
   isCnpjValid,
+  calculateFirstDigit,
+  calculateSecondDigit,
 };
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,24 +1,58 @@
-const { isCpfValid, isCnpjValid } = require("./index");
+const {
+  isCpfValid,
+  isCnpjValid,
+  calculateFirstDigit,
+  calculateSecondDigit,
+} = require("./index");
 
-describe("CPF Validation", () => {
-  test("Should remove non-digit characters from the CPF", () => {
-    const cpfWithMask = "123.456.789-09";
-    expect(isCpfValid(cpfWithMask)).toBe(true);
+describe("CPF Calculation and Validation", () => {
+  describe("Digit verification", () => {
+    test("Should correctly calculate the first digit of a valid CPF", () => {
+      const cpf = "111444777";
+      expect(calculateFirstDigit(cpf)).toBe(3);
+    });
+
+    test("Should correctly calculate the second digit of a valid CPF", () => {
+      const cpf = "111444777";
+      expect(calculateSecondDigit(cpf)).toBe(5);
+    });
+
+    test("Should return an incorrect first digit for an invalid CPF", () => {
+      const cpf = "123456789";
+      expect(calculateFirstDigit(cpf)).not.toBe(3);
+    });
+
+    test("Should return an incorrect second digit for an invalid CPF", () => {
+      const cpf = "123456789";
+      expect(calculateSecondDigit(cpf)).not.toBe(5);
+    });
   });
 
-  test("Should return false if CPF has less than 11 digits", () => {
-    const invalidCpf = "123.456.789";
-    expect(isCpfValid(invalidCpf)).toBe(false);
-  });
+  describe("isCpfValid", () => {
+    test("Should remove non-digit characters from the CPF", () => {
+      const cpfWithMask = "123.456.789-09";
+      expect(isCpfValid(cpfWithMask)).toBe(true);
+    });
 
-  test("Should return false if CPF has more than 11 digits", () => {
-    const invalidCpf = "123.456.789.0987";
-    expect(isCpfValid(invalidCpf)).toBe(false);
-  });
+    test("Should return false if CPF has less than 11 digits", () => {
+      const invalidCpf = "123.456.789";
+      expect(isCpfValid(invalidCpf)).toBe(false);
+    });
 
-  test("Should return true for a valid CPF with exactly 11 digits", () => {
-    const validCpf = "12345678909";
-    expect(isCpfValid(validCpf)).toBe(true);
+    test("Should return false if CPF has more than 11 digits", () => {
+      const invalidCpf = "123.456.789.0987";
+      expect(isCpfValid(invalidCpf)).toBe(false);
+    });
+
+    test("Should return true for a valid CPF with exactly 11 digits", () => {
+      const validCpf = "12345678909";
+      expect(isCpfValid(validCpf)).toBe(true);
+    });
+
+    test("Should return false for an invalid CPF with incorrect digits", () => {
+      const invalidCpf = "11144477736";
+      expect(isCpfValid(invalidCpf)).toBe(false);
+    });
   });
 });
 
