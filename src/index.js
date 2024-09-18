61affd50b2ffe993e9004afe65417bec8e8e3cc2
Danrley Pereira <danrleywillian@gmail.com>
2024-09-17 22:50:22 -0300

    feat: csv writer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import { processCsvToArray } from "./helpers/csvProcessor.js";
+import { saveProcessedCsv } from "./helpers/csvWriter.js";
 
 processCsvToArray("data.csv")
-  .then((data) => console.log(data))
+  .then((data) => {
+    // Salvar o novo CSV com os dados convertidos e validados
+    saveProcessedCsv(data, "processed_data.csv");
+  })
   .catch((error) => console.error(error));
