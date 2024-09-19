e55a9affb9d2dbdba8dd5e24f192956f62f18112
Danrley Pereira <danrleywillian@gmail.com>
2024-09-19 12:20:43 -0300

    feat(handling huge files): processing csvs using streams

--- src/helpers/csvProcessor.js
+++ src/helpers/csvProcessor.js
@@ -1,6 +1,9 @@
 import fs from "fs";
 import csv from "csv-parser";
+import csvParser from "csv-parser";
 
+// this function is used to process the CSV file and return an array of objects
+// this loads the entire file into memory, so it's not recommended for large files
 export const processCsvToArray = (filePath) => {
   return new Promise((resolve, reject) => {
     const results = [];
@@ -11,4 +14,14 @@ export const processCsvToArray = (filePath) => {
       .on("end", () => resolve(results))
       .on("error", (error) => reject(error));
   });
+};
+
+// this function is used to process the CSV file in a stream
+// this is recommended for large files, as it doesn't load the entire file into memory
+export const processCsvInStream = (filePath, onData, onEnd, onError) => {
+  fs.createReadStream(filePath)
+    .pipe(csvParser())
+    .on("data", onData)
+    .on("end", onEnd)
+    .on("error", onError);
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,57 @@
-import { processCsvToArray } from "./helpers/csvProcessor.js";
+import {
+  processCsvInStream,
+  processCsvToArray,
+} from "./helpers/csvProcessor.js";
 import { saveProcessedCsv } from "./helpers/csvWriter.js";
+import fs from "fs";
+import { isCpfValid } from "./validators/cpf.js";
+import { isCnpjValid } from "./validators/cnpj.js";
+import { validatePayments } from "./validators/paymentValidator.js";
+import { formatToBRL } from "./helpers/currencyConverter.js";
 
+// arquivos pequenos podem ser processados com processCsvToArray
 processCsvToArray("data.csv")
   .then((data) => {
     // Salvar o novo CSV com os dados convertidos e validados
     saveProcessedCsv(data, "processed_data.csv");
   })
   .catch((error) => console.error(error));
+
+const outputJSONStream = fs.createWriteStream("processed_data.json", {
+  flags: "a",
+});
+const outputCSVSream = fs.createWriteStream("processed_data.csv", {
+  flags: "a",
+});
+
+// arquivos grandes devem ser processados com processCsvInStream
+processCsvInStream("data.csv", onData, onEnd, onError);
+
+// hoisting para podermos definir as funções depois de chamá-las
+function onData(data) {
+  const processedData = {
+    ...data,
+    isValidCpfOrCNPJ: isCpfValid(data.nrCpfCnpj) || isCnpjValid(data.nrCpfCnpj),
+    isPaymentValid: validatePayments(data).isValid,
+    vlTotalBRL: formatToBRL(data.vlTotal),
+    vlPrestaBRL: formatToBRL(data.vlPresta),
+    vlMoraBRL: formatToBRL(data.vlMora),
+    vlMultaBRL: formatToBRL(data.vlMulta),
+    vlAtualBRL: formatToBRL(data.vlAtual),
+  };
+  const jsonData = JSON.stringify(data);
+  outputJSONStream.write(jsonData);
+
+  const csvLine = Object.values(data).join(",") + "\n";
+  outputCSVSream.write(csvLine);
+}
+
+function onEnd() {
+  outputJSONStream.end();
+  outputCSVSream.end();
+  console.log("CSV e JSON processados com sucesso!");
+}
+
+function onError(error) {
+  console.error(error);
+}
